45e59a648ad1a30855c9bed5643a81a98c582124
Vlad <deh456794@gmail.com>
2024-02-09 17:55:40 +0200

    dz_9

---
+++ dz_9/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// #1
+function one_to_ten():void
+{
+    $i = 1;
+    while ($i !== 11) {
+        echo $i . PHP_EOL;
+        $i++;
+    }
+}
+
+// #2
+function factorial(int $num_factorial) :int {
+    $i = 1;
+    $factor = 1;
+    while ($i !== $num_factorial+1){
+        $factor *= $i;
+        $i++;
+    }
+    return $factor;
+}
+
+// #3
+$arr = [];
+function paired_numbers(array $arr) :array
+{
+    for ($i = 1; $i <= 20; $i++){
+        if( $i % 2 === 0){
+            $arr[$i] = $i;
+        }
+    }
+    return $arr;
+}
+
+one_to_ten();
+echo "factorial = ". factorial(5) . PHP_EOL;
+$arr = paired_numbers($arr);
+print_r($arr);
